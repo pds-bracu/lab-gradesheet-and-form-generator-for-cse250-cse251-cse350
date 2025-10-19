75c7a15130bc3049c312011270327c70599510c1
pds-bracu <purbayandas121@gmail.com>
2025-10-19 13:22:24 +0600

    Update resources_and_info.js

--- resources_and_info.js
+++ resources_and_info.js
@@ -1,25 +1,35 @@
-var semester = "Summer 2025";
-var userSpreadsheetId = "1gZOXAFe_7TbV-WeuVg21c0F";
-var urlSpreadsheetId = "1ib4Xw_0WhWD-dVkd6jOT1N";
+var semester = "Fall 2025";
+var userSpreadsheetId = "1GgvR9vRk68b0s5l2F-pdIhcCs...";
+var urlSpreadsheetId = "1H4i1jf_CH2m_tixXQ7FURqPpD...";
 
 const templatesAllCourse = {
   "CSE250": {
-    gradesheetId: "1oEdfR3B8wmO-t2Cz8DwxFEqxmveMjj",
-    reportFormId: "1gj599_RAAW8f3O8Z",
-    destinationFolderId: "10du4hj4"
+    gradesheetId: "1nvMUS_6sdnDRwMe9a0er1IE9W10q_...",
+    reportFormId: "1QtQCRakEqgc8li6pdPmI...",
+    destinationFolderId: "1pvE4TbgYB31nJl..."
   },
   "CSE251": {
-    gradesheetId: "1nJHPHn09mN1Nw7LO4",
-    reportFormId: "1WANV7111zXNgBfXgcxThp",
-    destinationFolderId: "1NwzGTATo"
+    gradesheetId: "1PpTlVrv-N69iASREc5hwUM4mF7...",
+    reportFormId: "1qDhm12ixoNMu2td969jzZFYPlju...",
+    destinationFolderId: "1gLoGhqoO3HFABPPYdSFtGJhCGXpavkAC"
   },
   "CSE350": {
-    gradesheetId: "17zyhJKCD-vAar1e",
-    reportFormId: "118A_McgLEciifblJ",
-    destinationFolderId: "1FDF7Gp5SjV4Lo"
+    gradesheetId: "1l0SuoAIH9F5zQTq_wluo7DKmXm1...",
+    reportFormId: "1VOB3UnmiCFqZSsXeXv9xibofQ9...",
+    destinationFolderId: "1tmYYL1dD_sSLh0VykF..."
   },
 };
 
+var userEmail = Session.getActiveUser();
+var userSpreadsheet = SpreadsheetApp.openById(userSpreadsheetId);
+var userSheet = userSpreadsheet.getSheetByName("User");
+var processStatusCell = userSheet.getRange("D2");
+
+function getUI() {
+  return SpreadsheetApp.getUi();
+}
+
+// The following is optional
 const secConstraintsAllCourses = {
   "CSE250": {
     min: 1,
@@ -37,9 +47,3 @@ const secConstraintsAllCourses = {
     missing: []
   },
 };
-
-var userEmail = Session.getActiveUser();
-var userSpreadsheet = SpreadsheetApp.openById(userSpreadsheetId);
-var userSheet = userSpreadsheet.getSheetByName("User");
-var processStatusCell = userSheet.getRange("D2");
-var ui = SpreadsheetApp.getUi();
